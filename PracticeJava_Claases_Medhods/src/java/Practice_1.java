295da62f9c0144cc2a65ea396b519aace112eb8d
Egor Chichin <chichin@inbox.ru>
2025-02-17 22:55:29 +0300

    1 задача (fraction) решена

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_1.java
@@ -0,0 +1,14 @@
+public class Practice_1 {
+    public static void main(String[] args) {
+        System.out.println(fraction(5.3454));
+        System.out.println(fraction(2.75446763));
+        System.out.println(fraction(0.32));
+    }
+    public static double fraction(double x){
+        int y = (int) x;
+        double res = x - y;
+        String s = String.format("%.3f", res).replace(",", ".");  // округляем res до 3-х знаков после запятой и подменяем запятую на точку
+        //System.out.println(s);  // => 0,000
+        return Double.parseDouble(s);
+    }
+}
