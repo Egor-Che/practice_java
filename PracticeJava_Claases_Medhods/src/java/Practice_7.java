8d3177d67229d826b1cca2736acb63134f13e5b3
Egor Chichin <chichin@inbox.ru>
2025-02-20 20:08:03 +0300

    Решено задание 7

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_7.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class Practice_7 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите первое целое число: ");
+        int a = in.nextInt();
+        System.out.print("Пожалуйста, введите второе целое число: ");
+        int b = in.nextInt();
+        System.out.print("Пожалуйста, введите третье целое число: ");
+        int num = in.nextInt();
+        System.out.println("Принадлежит ли третье введенное число к заданному первым и вторым числом интервалу?");
+        isInRange(a,b,num);
+    }
+    public static boolean isInRange(int a, int b, int num){
+        boolean res = false;
+        if (a > b) {
+            if ((a > num) && (num > b)) {
+                res = true;
+            }
+        }
+        else if (a < b) {
+            if ((b > num) && (num > a)) {
+                res = true;
+            }
+        }
+        else {
+            res = false;
+        }
+        System.out.println(res);
+        return res;
+    }
+}
