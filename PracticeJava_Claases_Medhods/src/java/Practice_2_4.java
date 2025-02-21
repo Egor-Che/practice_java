5f422d9802ad6bee2279772173494cfb68088934
Egor Chichin <chichin@inbox.ru>
2025-02-21 19:18:18 +0300

    Практика Java 2. Условия #4 - решено

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_2_4.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Practice_2_4 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите первое целое число: ");
+        int x = in.nextInt();
+        System.out.print("Пожалуйста, введите второе целое число: ");
+        int y = in.nextInt();
+        System.out.println(makeDecision(x, y));
+    }
+    public static String makeDecision(int x, int y){
+        System.out.println("x=" + x + " y=" + y);
+        if (x > y){
+            System.out.println("Результат: “" + x + " > " + y + "”");
+        }
+        else if (x < y){
+            System.out.println("Результат: “" + x + " < " + y + "”");
+        }
+        else{
+            System.out.println("Результат: “" + x + " == " + y + "”");
+        }
+        return " ";
+    }
+}
