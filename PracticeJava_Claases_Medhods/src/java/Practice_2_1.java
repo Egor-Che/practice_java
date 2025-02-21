12ef2b94aa7c85e0d29c6310464d097a0a972716
Egor Chichin <chichin@inbox.ru>
2025-02-21 16:08:22 +0300

    Практика Java 2. Условия #1 - решено

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_2_1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Practice_2_1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите целое число: ");
+        int x = in.nextInt();
+        System.out.println(abs(x));
+    }
+    public static int abs(int x){
+        if (x < 0){
+            x = -1 * x;
+        }
+        return x;
+    }
+}
