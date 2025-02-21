80d6e65d701f7ac43d3ca18b718235f759a56708
Egor Chichin <chichin@inbox.ru>
2025-02-21 20:25:05 +0300

    Практика Java 2. Условия #10 - решено

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_2_10.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Practice_2_10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите целое число: ");
+        int x = in.nextInt();
+        System.out.println(age(x));
+    }
+    public static String age(int x){
+        System.out.println("x=" + x);
+        System.out.print("Результат: ");
+        if ((x % 10 == 1) && (x % 100 != 11)){
+            return x + " год";
+        }
+        else if (((x%10 == 2) && (x%100 != 12)) || ((x%10 == 3) && (x%100 != 13)) || ((x%10 == 4) && (x%100 != 14))){
+            return x + " годa";
+        }
+        return x + " лет";
+    }
+}
