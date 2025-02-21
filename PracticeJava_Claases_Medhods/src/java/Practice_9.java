fc05a7f66e3e098395d7ebf9e0fa0559b09b3470
Egor Chichin <chichin@inbox.ru>
2025-02-21 15:48:54 +0300

    Решена задача 9

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_9.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Practice_9 {
+    static boolean res = false;
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите первое целое число: ");
+        int a = in.nextInt();
+        System.out.print("Пожалуйста, введите второе целое число: ");
+        int b = in.nextInt();
+        System.out.print("Пожалуйста, введите третье целое число: ");
+        int c = in.nextInt();
+        System.out.println(isEqual(a, b, c));
+    }
+    public static boolean isEqual (int a, int b, int c){
+        if ((a == b) && (b == c)){
+            res = true;
+        }
+        return res;
+    }
+}
