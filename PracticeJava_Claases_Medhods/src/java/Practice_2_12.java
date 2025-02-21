8e3b014fe6652817b01323c16f93d75cd77614a7
Egor Chichin <chichin@inbox.ru>
2025-02-21 21:25:00 +0300

    Практика Java 2. Условия #12 - решено

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_2_12.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class Practice_2_12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите название дня недели: ");
+        String str = in.next();
+        System.out.println(getResult(str));
+    }
+    public static String getResult(String str){
+        System.out.println("x=" + str);
+        System.out.println("Результат:");
+        printDays(str);
+        return "";
+    }
+    public static void printDays(String x){
+        switch (x){
+            case "Понедельник" : System.out.println(" Понедельник");
+            case "Вторник" : System.out.println(" Вторник");
+            case "Среда" : System.out.println(" Среда");
+            case "Четверг": System.out.println(" Четверг");
+            case "Пятница" : System.out.println(" Пятница");
+            case "Суббота" : System.out.println(" Суббота");
+            case "Воскресенье" : System.out.println(" Воскресенье");break;
+            default : System.out.println(" - это не день недели");break;
+        }
+    }
+}
