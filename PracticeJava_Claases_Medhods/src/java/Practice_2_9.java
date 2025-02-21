3090e9d1a76dc56392ac71691f60ac325bcbdbd3
Egor Chichin <chichin@inbox.ru>
2025-02-21 20:13:28 +0300

    Практика Java 2. Условия #9 - решено

---
+++ PracticeJava_Claases_Medhods/src/java/Practice_2_9.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Practice_2_9 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Пожалуйста, введите первое целое число: ");
+        int x = in.nextInt();
+        System.out.print("Пожалуйста, введите второе целое число: ");
+        int y = in.nextInt();
+        System.out.println(magic6(x , y));
+    }
+    public static boolean magic6(int x, int y){
+        System.out.println("x=" + x + " y=" + y);
+        System.out.print("Результат: ");
+        if ((x == 6) || (y == 6) || (x+y == 6) || (x-y ==6) || (y-x ==6)){
+            return true;
+        }
+        return false;
+    }
+}
