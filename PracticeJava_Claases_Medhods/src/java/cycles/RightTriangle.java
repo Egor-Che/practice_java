1f91436e85779c351e9bc4033bb7c8ca684a37fe
Egor Chichin <chichin@inbox.ru>
2025-02-28 12:52:52 +0300

    5 раздел. Практика Java. Циклы #9 - RightTriangle - решено

--- PracticeJava_Claases_Medhods/src/java/cycles/RightTriangle.java
+++ PracticeJava_Claases_Medhods/src/java/cycles/RightTriangle.java
@@ -1,24 +1,26 @@
 import java.util.Scanner;
-public class LeftTriangle {
+
+public class RightTriangle {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.print("Пожалуйста, введите целое положительное число: ");
         int x = in.nextInt();
-        leftTriangle(x);
+        rightTriangle(x);
     }
-    public static void leftTriangle(int x){
+    public static void rightTriangle(int x){
         if (x < 1) {
             System.out.println("Вы ввели неверные данные");
         }
-        else {
-            for (int i = 1; i <= x; i++) {
-                System.out.print("*");
-                for (int a = 1; a < i; a++) {
-                    System.out.print("*");
-                }
-                System.out.println(" ");
+        int a = 1;
+        int b = 1;
+        for (int i = 1; i <= a; i++) {
+            for (a = i; a <= x-1 ; a++){
+                System.out.print(" ");
             }
+            for (b = 1; b <= i ; b++){
+                System.out.print("*");
+            }
+            System.out.println("");
         }
     }
 }
-
