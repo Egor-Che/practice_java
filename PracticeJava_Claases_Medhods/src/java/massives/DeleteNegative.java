903c9115a34bfea8259190af8cf0ec57a6e4978e
Egor Chichin <chichin@inbox.ru>
2025-03-03 10:35:03 +0300

    5 раздел. Практика Java. Массивы #10 - DeleteNegative - исправлено

--- PracticeJava_Claases_Medhods/src/java/massives/DeleteNegative.java
+++ PracticeJava_Claases_Medhods/src/java/massives/DeleteNegative.java
@@ -1,14 +1,16 @@
 import java.util.Arrays;
 public class DeleteNegative {
     public static void main(String[] args) {
-        int[] arr = new int[]{1,2,-3,4,-2,2,-5};
+        int[] arr = new int[]{1,2,-3,4,-2,2,-5,0};
+        System.out.println(Arrays.toString(arr));//Исходный массив
         deleteNegative(arr);
+        System.out.println(Arrays.toString(deleteNegative(arr)));//массив с удаленными негативными числами
     }
     public static int[] deleteNegative(int[] arr){
         int [] pos = new int[arr.length];
         int count = 0;
         for (int i = 0 ; i < arr.length ; i ++) {
-            if (arr[i] > 0){
+            if (arr[i] >= 0){
                 pos[i] = arr[i];
                 count ++;
             }
@@ -21,7 +23,6 @@ public class DeleteNegative {
                 y++;
             }
         }
-        System.out.println(Arrays.toString(arr));
         return arr;
     }
 }
