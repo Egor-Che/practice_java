25234f16c16d411bf645c2cb53e98b32c168f806
Egor Chichin <chichin@inbox.ru>
2025-03-03 11:02:03 +0300

    5 раздел. Практика Java. Массивы #12 - AddIns - исправлено

--- PracticeJava_Claases_Medhods/src/java/massives/AddIns.java
+++ PracticeJava_Claases_Medhods/src/java/massives/AddIns.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 public class AddIns {
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5};
-        int[] ins = new int[]{7,8,9};
+        int[] ins = new int[]{7,8,9,89,76,-9,2};
         int pos = 3;
         int[] newArr = addIns(arr, ins, pos);
         System.out.println(Arrays.toString(newArr));
@@ -16,11 +16,12 @@ public class AddIns {
         for (; i < pos; i++){
             newArr[i] = arr[i];
         }
-        for (int a = 0; a < len2 ; a++){
-            newArr[pos+a] = ins[a];
+        for (int j = 0; j < len2 ; j++){
+            newArr[pos+j] = ins[j];
         }
         for (i = pos+len2; i < lenAll ; i++){
-            newArr[i] = arr[i-pos];
+            newArr[i] = arr[pos];
+            pos = pos+1;
         }
         return newArr;
     }
