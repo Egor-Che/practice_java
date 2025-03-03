067096f20034a259b731a3511dc00080262f207d
Egor Chichin <chichin@inbox.ru>
2025-03-03 10:26:57 +0300

    5 раздел. Практика Java. Массивы #6 - Reverse - исправлено еще раз

--- PracticeJava_Claases_Medhods/src/java/massives/Reverse.java
+++ PracticeJava_Claases_Medhods/src/java/massives/Reverse.java
@@ -1,14 +1,17 @@
+import java.util.Arrays;
 public class Reverse {
+
     public static void main(String[] args) {
         int[] arr = new int[]{1,2,3,4,5,6,9};
+        System.out.println(Arrays.toString(arr));//исходный массив
         reverse(arr);
+        System.out.println(Arrays.toString(arr));//массив после реверса
     }
     public static void reverse(int[] arr){
-        int i = arr.length - 1;
-        while (i >= 0) {
-            System.out.print(arr[i] + " ");
-            i--;
-            if (i < 0) break;
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = temp;
         }
     }
 }
